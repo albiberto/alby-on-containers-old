78caabfeaf9fa59a97a5f67142fa8d25ee39effe
Alberto Viezzi <aviezzi@outlook.com>
2021-02-21 16:11:20 +0100

    Move serilog to config

--- src/AlbyOnContainers.IdentityServer/Program.cs
+++ src/AlbyOnContainers.IdentityServer/Program.cs
@@ -1,55 +1,25 @@
 using System;
-using System.IO;
 using System.Threading.Tasks;
 using IdentityServer.Infrastructure;
 using IdentityServer4.EntityFramework.DbContexts;
 using Libraries.IHostExtensions;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
-using Serilog.Events;
 
 namespace IdentityServer
 {
     static class Program
     {
-        static readonly string Env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-
-        static IConfiguration Configuration { get; } = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", true, true)
-            .AddEnvironmentVariables()
-            .Build();
-
         static async Task Main(string[] args)
         {
-            var minLevel = string.Equals(Env, "Debug", StringComparison.InvariantCultureIgnoreCase) ||
-                           string.Equals(Env, "Development", StringComparison.InvariantCultureIgnoreCase)
-                ? LogEventLevel.Information
-                : LogEventLevel.Warning;
-
-            Log.Logger = new LoggerConfiguration()
-                .Enrich.WithMachineName()
-                .Enrich.WithEnvironmentName()
-                .Enrich.WithEnvironmentUserName()
-                .Enrich.WithApplicationName()
-                .WriteTo.Console()
-                .WriteTo.Seq(Configuration["Seq:Host"], minLevel)
-                .CreateLogger();
-
             try
             {
+                var host = CreateHostBuilder(args).Build();
                 Log.Information("IdentityServer Starting");
 
-                var host = CreateHostBuilder(args).Build();
-
-                await host.MigrateAsync<ApplicationDbContext>(async (context, services) =>
-                    await new ApplicationDbContextSeed().SeedAsync(context));
-                await host.MigrateAsync<ConfigurationDbContext>(async (context, _) =>
-                {
-                    await new ConfigurationDbContextSeed().SeedAsync(context);
-                });
+                await host.MigrateAsync<ApplicationDbContext>(async (context, services) => await new ApplicationDbContextSeed().SeedAsync(context));
+                await host.MigrateAsync<ConfigurationDbContext>(async (context, _) => { await new ConfigurationDbContextSeed().SeedAsync(context); });
                 await host.MigrateAsync<PersistedGrantDbContext>((_, __) => Task.CompletedTask);
 
                 await host.RunAsync();
@@ -73,17 +43,24 @@ namespace IdentityServer
         static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((context, builder) =>
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+                .ConfigureAppConfiguration((host, builder) =>
                 {
-                    if (context.HostingEnvironment.IsDevelopment())
+                    if (host.HostingEnvironment.IsDevelopment())
                     {
                         // Add by default in Development
                         // builder.AddUserSecrets(Assembly.GetExecutingAssembly());
                     }
                 })
-                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
-                .ConfigureLogging(builder => builder.AddSerilog(Log.Logger, true))
-                .UseSerilog();
+                .ConfigureLogging((host, builder) =>
+                {
+                    Log.Logger = new LoggerConfiguration()
+                        .ReadFrom.Configuration(host.Configuration)
+                        .CreateLogger();
+
+                    builder.AddSerilog(Log.Logger, true);
+                });
         }
     }
 }
