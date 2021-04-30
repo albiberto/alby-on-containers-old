f15d926685f522b2d5d67e3c5ed5fb19af8c9945
Alberto Viezzi <aviezzi@outlook.com>
2021-04-30 23:15:50 +0200

    Add token in HttpRequest

---
+++ src/AlbyOnContainers.Tools.Catalog/Model/ProductType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlbyOnContainers.Tools.Catalog.Model
+{
+    public class ProductType
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; }
+    }
+
+    public class ProductsType
+    {
+        public IEnumerable<ProductType> Products { get; init; }
+    }
+}
--- src/AlbyOnContainers.Tools.Catalog/Program.cs
+++ src/AlbyOnContainers.Tools.Catalog/Program.cs
@@ -1,6 +1,10 @@
 using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.SystemTextJson;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,12 +13,29 @@ namespace AlbyOnContainers.Tools.Catalog
 {
     public class Program
     {
+        private static HttpMessageHandler BuildHttpMessageHandler(IServiceProvider provider) =>
+            provider.GetService<AuthorizationMessageHandler>()?.ConfigureHandler(authorizedUrls: new[] {"https://localhost:5001"}, scopes: new[] {"companyApi"});
+
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+            builder.Services.AddScoped(sp => new GraphQLHttpClient( new GraphQLHttpClientOptions
+            {
+                EndPoint =  new Uri("http://localhost:5004/graphql"),
+                PreprocessRequest = async (request, client) =>
+                {
+                    var provider = sp.GetRequiredService<IAccessTokenProvider>();
+                    var tokenResult = await provider.RequestAccessToken();
+
+                    var result = tokenResult.TryGetToken(out var token);
+
+                    if(result) client.HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Value);
+                    
+                    return new GraphQLHttpRequest(request);
+                }
+            }, new SystemTextJsonSerializer()));
 
             builder.Services.AddOidcAuthentication(options =>
             {
